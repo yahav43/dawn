db0021ef7df5c421f869fa3ea9e7d4f738bb38bc
Your New Username <yhonig43@gmail.com>
2024-12-13 12:54:58 +0200

    set autoplay

--- assets/global.js
+++ assets/global.js
@@ -704,8 +704,8 @@ class DeferredMedia extends HTMLElement {
       content.appendChild(this.querySelector('template').content.firstElementChild.cloneNode(true));
 
       this.setAttribute('loaded', true);
-      this.setAttribute('autoplay', true);
       const deferredElement = this.appendChild(content.querySelector('video, model-viewer, iframe'));
+      deferredElement.play();
       if (focus) deferredElement.focus();
       if (deferredElement.nodeName == 'VIDEO' && deferredElement.getAttribute('autoplay')) {
         // force autoplay for safari
