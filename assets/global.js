f1debd32d902c92b2bcf56b4e535c236bc95f526
Your New Username <yhonig43@gmail.com>
2024-12-13 12:52:33 +0200

    make video autoplay

--- assets/global.js
+++ assets/global.js
@@ -704,6 +704,7 @@ class DeferredMedia extends HTMLElement {
       content.appendChild(this.querySelector('template').content.firstElementChild.cloneNode(true));
 
       this.setAttribute('loaded', true);
+      this.setAttribute('autoplay', true);
       const deferredElement = this.appendChild(content.querySelector('video, model-viewer, iframe'));
       if (focus) deferredElement.focus();
       if (deferredElement.nodeName == 'VIDEO' && deferredElement.getAttribute('autoplay')) {
